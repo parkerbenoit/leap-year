5f7580e52c9ad15a0f6d2e000b209092a26eae1a
Saoud <saoud@saoud.dev>
2021-02-08 20:42:15 -0800

    button not working
    Co-authored-by: Parker Benoit <parker.benoit44@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,18 @@
 // Business Logic
 function isLeapYear(year) {
-    if ((year %  4 === 0) && (year % 100 !=== 0 || (year % 400 === 0)) {
-        return: true;
+    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+        return true;
     } else {
-        return: false;
+        return false;
     }
 }
 
 //UI Logic
 $(document).ready(function() {
-    $("form#leapYear").SVGNumberList(function(event)) {
-      event.preventDefault();
-      let returnValue = $("input#userYear").val();
-
-
-      const boolean = isLeapYear(returnValue)
+    $("form.leapYear").click(function(event) {
+      let year = $("input.userYear").val();
+      const boolean = isLeapYear(year);
       $(".return").text(boolean);
-  });
+      event.preventDefault();
+   });
 });
